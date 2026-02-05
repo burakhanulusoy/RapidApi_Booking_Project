ebb0561c99852c4c3ca4dae5b64c42e8131f540d
Burak Han Ulusoy <burakhanulusoy18@gmail.com>
2026-02-05 12:36:22 +0300

    Add project files.

---
+++ MyAcademy_RapidApi_Booking/Controllers/DefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyAcademy_RapidApi_Booking.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MyAcademy_RapidApi_Booking/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyAcademy_RapidApi_Booking.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
